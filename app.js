8228fba3cb75b3b77305944ed21cac4777ef67cc
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-19 11:58:15 +0100

    adjusted the drawing line

--- app.js
+++ app.js
@@ -4,31 +4,42 @@ const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const sizeEl = document.getElementById("size");
 const colorEl = document.getElementById("color");
-let size = 20;
-let x = 50;
-let y = 50;
+let size = 30;
+let color = "black";
+let x = undefined;
+let y = undefined;
 let isPressed = false;
 
 // the mouse is pressdown
-canvas.addEventListener("mousedown", () => {
+canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
 });
 
 // the mouse is pressup
-canvas.addEventListener("mouseup", () => {
+canvas.addEventListener("mouseup", (e) => {
   isPressed = false;
+
+  x = undefined;
+  y = undefined;
 });
 
 // starting and continue drawing the circle
 canvas.addEventListener("mousemove", (e) => {
   if (isPressed) {
-    const x = e.offSetX;
-    const y = e.offSetY;
-    drawCircle(x, y);
+    const x2 = e.offSetX;
+    const y2 = e.offSetY;
+
+    drawCircle(x2, y2); //make the line thick
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
   }
 });
 
-// drawing a circle
+// drawing a circle/arc
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
@@ -36,9 +47,19 @@ function drawCircle(x, y) {
   ctx.fillStyle = color;
 }
 
+// drawing line
+drawLine(100, 100, 200, 200);
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color; //targeting the color selector
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
 // the increase button
 increaseBtn.addEventListener("click", () => {
-  size += 5;
+  size += 5; //make the line big
   if (size > 50) {
     size = 0;
   }
@@ -47,7 +68,7 @@ increaseBtn.addEventListener("click", () => {
 
 // the decrease button
 decreaseBtn.addEventListener("click", () => {
-  size -= 5;
+  size -= 5; //make the line small
   if (size < 5) {
     size = 5;
   }
@@ -64,13 +85,3 @@ function updateSizeOnScreen() {
 colorEl.addEventListener("change", (e) => {
   color = e.target.value;
 });
-
-// drawCircle();
-// continue drawing circle
-
-// function draw() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   drawCircle(x, y);
-//   requestAnimationFrame(draw);
-// }
-// draw();
